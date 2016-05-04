a90dd73970c3dbcedfc499e3dee14c29140184df
Danny Tunon <danieltunon@gmail.com>
2016-05-03 18:53:04 -0700

    Add Tweet component

--- public/client/components/Tweet.js
+++ public/client/components/Tweet.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import styles from '../styles/main.css';
 
-const Tweet = () => {
-    return (
-      <div> Tweet </div>
-    );
-};
+const Tweet = () => (
+  <div className={styles.tweet}>
+    <h1>Tweet</h1>
+    <section>
+      <button>Post</button>
+      <span> </span>
+      <button>Edit</button>
+      <span> </span>
+      <button>Trash</button>
+    </section>
+    <section>
+      <p>This is where the generated tweet will live. Super cool huh?</p>
+    </section>
+  </div>
+);
 
 export default Tweet;
