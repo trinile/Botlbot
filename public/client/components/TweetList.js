092cd65c9a2b79a4202a944738a9458b494dde0b
Danny Tunon <danieltunon@gmail.com>
2016-05-05 15:08:40 -0700

    Implement TweetsList Component

--- public/client/components/TweetList.js
+++ public/client/components/TweetList.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
+import Tweet from './Tweet';
 import styles from '../styles/main.css';
 
 
-class TweetsList extends Component {
+const TweetsList = ({ tweets }) => (
+  <main styles={styles['tweets-list']}>
+    {tweets.map((t) => (
+      <Tweet
+        key={t.id_str}
+        {...t}
+      />
+    ))}
+  </main>
+);
 
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
-
+TweetsList.propTypes = {
+  tweets: React.PropTypes.arrayOf(React.PropTypes.element),
 };
 
 export default TweetsList;
