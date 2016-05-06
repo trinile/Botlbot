aa29a201461ea9e5c2cc2ae0294aa785a9ddc242
Danny Tunon <danieltunon@gmail.com>
2016-05-05 20:42:40 -0700

    Modify main reducer file to accept tweets reducer

--- public/client/reducers/index.js
+++ public/client/reducers/index.js
@@ -2,13 +2,15 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 // Reducers
-import AuthReducer  from './Auth.js';
+import AuthReducer from './Auth.js';
+import tweets from './tweets';
 
 const Reducers = combineReducers({
   AuthReducer,
+  tweets,
   routing: routerReducer,
 });
 
 // { LoginReducer: { LoginReducer: ..... , LogoutReducer, Routing::; }
-console.log('Reducers ', Reducers.LoginReducer);
+// console.log('Reducers ', Reducers.LoginReducer);
 export default Reducers;
