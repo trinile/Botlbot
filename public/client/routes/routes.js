2a9349e1ebd21b147800ec1a8748295784dc3a4c
Danny Tunon <danieltunon@gmail.com>
2016-05-05 20:40:57 -0700

    Add imports and dummy path for About

--- public/client/routes/routes.js
+++ public/client/routes/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Route from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import Dashboard from '../components/Dashboard';
 import Main from '../containers/Main';
+import Splash from '../components/Splash';
 
 const Routes = (
   <Route>
     <Route path="/" component={Main}>
       <IndexRoute component={Splash} />
+      <Route path="/about" component={Dashboard} />
       <Route path="/dashboard" component={Dashboard} />
     </Route>
   </Route>
