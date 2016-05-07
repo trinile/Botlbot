81b1ecf63d483e032896026e7819ebba24e81089
Danny Tunon <danieltunon@gmail.com>
2016-05-07 12:07:10 -0700

    Add test for TRASH and POST tweet actions

--- test/reducers.js
+++ test/reducers.js
@@ -3,28 +3,28 @@ import deepFreeze from 'deep-freeze';
 
 import tweets from '../public/client/reducers/tweets';
 
-describe('Reducers', function(){
+describe('Tweets Reducers', function(){
 
-  describe('Tweets Reducers', function() {
+  describe('Adding Tweets', function() {
     const stateBefore1 = [];
     const stateBefore2 = [
-      { id: 0, text: 'not empty in here' },
+      { id_str: 0, text: 'not empty in here', posted: false, trashed: false },
     ];
     const action = {
       type: 'ADD_NEW_TWEETS',
       tweets: [
-        { id: 1, text: 'new tweet' },
-        { id: 2, text: 'another tweet' },
+        { id_str: 1, text: 'new tweet' },
+        { id_str: 2, text: 'another tweet' },
       ],
     };
     const stateAfter1 = [
-      { id: 1, text: 'new tweet' },
-      { id: 2, text: 'another tweet' },
+      { id_str: 1, text: 'new tweet', posted: false, trashed: false },
+      { id_str: 2, text: 'another tweet', posted: false, trashed: false },
     ];
     const stateAfter2 = [
-      { id: 0, text: 'not empty in here' },
-      { id: 1, text: 'new tweet' },
-      { id: 2, text: 'another tweet' },
+      { id_str: 0, text: 'not empty in here', posted: false, trashed: false },
+      { id_str: 1, text: 'new tweet', posted: false, trashed: false },
+      { id_str: 2, text: 'another tweet', posted: false, trashed: false },
     ];
 
     before('calls deep freeze to ensure no mutations', function() {
@@ -40,6 +40,65 @@ describe('Reducers', function(){
     it('Should add new tweets to a non-empty state', function() {
       expect(tweets(stateBefore2, action)).to.deep.equal(stateAfter2);
     });
+  });
 
+  describe('Posting Tweets', function() {
+    const stateBefore = [
+      { id_str: 0, text: 'not empty in here', posted: false, trashed: false },
+      { id_str: 1, text: 'new tweet', posted: false, trashed: false },
+      { id_str: 2, text: 'another tweet', posted: false, trashed: false },
+    ];
+    const action = {
+      type: 'POST_TWEET',
+      id: 1,
+    };
+    const stateAfter = [
+      { id_str: 0, text: 'not empty in here', posted: false, trashed: false },
+      { id_str: 1, text: 'new tweet', posted: true, trashed: false },
+      { id_str: 2, text: 'another tweet', posted: false, trashed: false },
+    ];
+
+    before('calls deep freeze to ensure no mutations', function() {
+      deepFreeze(stateBefore);
+      deepFreeze(action);
+    });
+
+    it('Should toggle a tweet as posted', function() {
+      expect(tweets(stateBefore, action)).to.have.deep.property('[1].posted', true);
+    });
+
+    it('Should not remove the posted tweet from the state', function() {
+      expect(tweets(stateBefore, action)).to.deep.equal(stateAfter);
+    });
+  });
+
+  describe('Trashing Tweets', function() {
+    const stateBefore = [
+      { id_str: 0, text: 'not empty in here', posted: false, trashed: false },
+      { id_str: 1, text: 'new tweet', posted: false, trashed: false },
+      { id_str: 2, text: 'another tweet', posted: false, trashed: false },
+    ];
+    const action = {
+      type: 'TRASH_TWEET',
+      id: 2,
+    };
+    const stateAfter = [
+      { id_str: 0, text: 'not empty in here', posted: false, trashed: false },
+      { id_str: 1, text: 'new tweet', posted: false, trashed: false },
+      { id_str: 2, text: 'another tweet', posted: false, trashed: true },
+    ];
+
+    before('calls deep freeze to ensure no mutations', function() {
+      deepFreeze(stateBefore);
+      deepFreeze(action);
+    });
+
+    it('Should toggle a tweet as trashed', function() {
+      expect(tweets(stateBefore, action)).to.have.deep.property('[2].trashed', true);
+    });
+
+    it('Should not remove the trashed tweet from the state', function() {
+      expect(tweets(stateBefore, action)).to.deep.equal(stateAfter);
+    });
   });
 });
