8d62d892ec9d7780b1cf5f90f769786eb905130f
Nathaniel Edwards <nedwards@gmail.com>
2016-05-09 14:23:54 -0700

    Add route and link for Build-a-Bot view

---
+++ public/client/components/BuildABot.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BuildABot = () => (
+  <main>
+    <h1>Build a bot!</h1>
+  </main>
+);
+
+export default BuildABot;
--- public/client/routes/routes.js
+++ public/client/routes/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Dashboard from '../containers/Dashboard';
 import MainContainer from '../containers/Main';
+import BuildABot from '../components/BuildABot';
 import Splash from '../components/Splash';
 import SideMenu from '../components/SideMenu';
 import PostedTweets from '../containers/PostedTweets';
@@ -12,6 +13,7 @@ const Routes = (
       <Route path="/about" components={{main: Dashboard}}/>
       <Route path="/dashboard" components={{main: Dashboard, sidebar: SideMenu}} />
       <Route path="/postedTweets" components={{main: PostedTweets, sidebar: SideMenu}}/>
+      <Route path="/build" component={BuildABot} />
     </Route>
 );
 
