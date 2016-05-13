c23a4bdaf63bdb1b4c313154a437b01db2d8f6aa
Nathaniel Edwards <nedwards@gmail.com>
2016-05-12 23:08:17 -0700

    Update addbutton to be more flexible

--- public/client/components/AddButton.js
+++ public/client/components/AddButton.js
@@ -2,9 +2,23 @@ import React, { PropTypes } from 'react';
 // import styles from '../styles/template.css';
 import {FloatingActionButton} from 'material-ui';
 import ContentAdd from 'material-ui/svg-icons/content/add';
+import ContentClear from 'material-ui/svg-icons/content/clear';
+import ActionDone from 'material-ui/svg-icons/action/done';
+import HardwareBack from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
 
-const AddButton = ({onClick}) => (
-  <FloatingActionButton onClick={onClick} mini={true}><ContentAdd/></FloatingActionButton>
-);
+const AddButton = ({onClick, icon}) => {
+  if(icon === 'add' || icon === undefined) {
+    return <FloatingActionButton onClick={onClick} mini={true}><ContentAdd/></FloatingActionButton>
+  }
+  if(icon === 'cancel') {
+    return <FloatingActionButton onClick={onClick} mini={true}><ContentClear/></FloatingActionButton>
+  }
+  if(icon === 'save') {
+    return <FloatingActionButton onClick={onClick} mini={true}><ActionDone/></FloatingActionButton>
+  }
+  if(icon === 'back') {
+    return <FloatingActionButton onClick={onClick} mini={true}><HardwareBack/></FloatingActionButton>
+  }
+};
 
 export default AddButton;
