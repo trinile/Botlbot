d36a6236f9fa7457e0537c68d017baffb12803be
Danny Tunon <danieltunon@gmail.com>
2016-05-13 11:58:27 -0700

    Refactor twitHelper to be explicit about parameters

--- server/helpers/twitHelper.js
+++ server/helpers/twitHelper.js
@@ -3,13 +3,13 @@ const User = require('./db/controllers/userController');
 const Promise = require('bluebird');
 
 function createTwit(token, secret) {
-  return new Twit({
+  return Promise.resolve(new Twit({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: token,
     access_token_secret: secret,
     timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-  });
+  }));
 }
 
 function postAsync(twit, status) {
@@ -26,10 +26,11 @@ function postAsync(twit, status) {
   });
 }
 
-function postTweet(data) {
-  return Promise.resolve(createTwit(data.token, data.tokenSecret))
-  .then(twit => postAsync(twit, data.tweet_text))
+function post(token, tokenSecret, status) {
+  return createTwit(token, tokenSecret)
+  .then(twit => postAsync(twit, status))
   .catch(err => console.log(err));
 }
 
-exports.postTweet = postTweet;
+
+export default twit = { post: post };
