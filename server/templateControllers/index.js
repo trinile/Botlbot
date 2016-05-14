899aba8647f53ec611ee7650b4643615567ee3fd
Danny Tunon <danieltunon@gmail.com>
2016-05-14 12:06:53 -0700

    Begin combining different chunkHandlers in one func

--- server/templateControllers/index.js
+++ server/templateControllers/index.js
@@ -1,8 +1,17 @@
+const Promise = require('bluebird');
 const chunkHandler = {
   'my feed': require('./myFeed'),
   text: require('./text'),
 };
 
-function parseTemplate(template) {
-  template.map( chunk => chunkHandler)
+function zip(arrays) {
+  const result = [];
+  arrays.forEach(arary => arary.forEach((item, i) => {
+    result[i] = (result[i] || []).concat(item);
+  }));
+  return result;
+}
+
+function parseTemplate(template, userId) {
+  return zip(Promise.all(template.map(chunk => chunkHandler[chunk.type](chunk.params, userId))));
 }
