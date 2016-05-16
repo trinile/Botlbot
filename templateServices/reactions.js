0a0e6cb6478d81807ce5d22ebe8e6e3ec56ddd80
Danny Tunon <danieltunon@gmail.com>
2016-05-16 13:04:07 -0700

    Implement emoji reaction service

---
+++ templateServices/reactions.js
@@ -0,0 +1,24 @@
+const emoji = require('./helpers/emoji');
+const _ = require('lodash');
+
+function randomValue(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function getIndexCache(keyword) {
+  return emoji.indexCache[keyword];
+}
+
+const getEmoji = _.flow(getIndexCache, randomValue, (i) => emoji.list[i])
+
+function getUnicodeFromEmoji(emoji) {
+  return emoji.data.unicode;
+}
+
+function formatUnicodeForHTML(unicode) {
+  return `&#x${unicode};`;
+}
+
+const getEmojiUnicode = _.flow(getEmoji, getUnicodeFromEmoji, formatUnicodeForHTML);
+
+module.exports = getEmojiUnicode;
