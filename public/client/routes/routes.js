9e00437df9006bb50eeaa2f0d5e5b8a555b0ab0d
Nathaniel Edwards <nedwards@gmail.com>
2016-05-16 23:51:40 -0700

    Create container for SideMenu and dynamically create children for 'Edit Templates' option

--- public/client/routes/routes.js
+++ public/client/routes/routes.js
@@ -5,17 +5,18 @@ import MainContainer from '../containers/Main';
 import BuildABot from '../components/BuildABot';
 import EditABot from '../components/EditABot';
 import Splash from '../components/Splash';
-import SideMenu from '../components/SideMenu';
+// import SideMenu from '../components/SideMenu';
+import SideMenuContainer from '../containers/SideMenuContainer';
 import PostedTweets from '../containers/PostedTweets';
 
 const Routes = (
     <Route path="/" component={MainContainer}>
       <IndexRoute components={{ main: Splash }} />
       <Route path="/about" components={{main: Dashboard}}/>
-      <Route path="/dashboard" components={{main: Dashboard, sidebar: SideMenu}} />
-      <Route path="/postedTweets" components={{main: PostedTweets, sidebar: SideMenu}}/>
-      <Route path="/build" components={{main: BuildABot, sidebar: SideMenu}} />
-      <Route path="/edit" components={{main: EditABot, sidebar: SideMenu}} />
+      <Route path="/dashboard" components={{main: Dashboard, sidebar: SideMenuContainer}} />
+      <Route path="/postedTweets" components={{main: PostedTweets, sidebar: SideMenuContainer}}/>
+      <Route path="/build" components={{main: BuildABot, sidebar: SideMenuContainer}} />
+      <Route path="/edit" components={{main: EditABot, sidebar: SideMenuContainer}} />
     </Route>
 );
 
