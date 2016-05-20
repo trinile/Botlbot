ad7dfd58c16349251b28cb8e0111ae4588c1d248
Nathaniel Edwards <nedwards@gmail.com>
2016-05-20 14:29:09 -0700

    Improve dashboard controls styling

--- public/client/components/DashboardControls.js
+++ public/client/components/DashboardControls.js
@@ -10,7 +10,7 @@ const DashboardControls = ({
   refreshTweets
 }) => {
   return (
-    <div>
+    <div style={{marginTop: '1rem'}}>
       <AddButton icon={'back'} onClick={() => { decrementPage(); refreshTweets(page - 1); }} disabled={page === 0} />
       <AddButton icon={'refresh'} onClick={() => { resetPage(); refreshTweets(0); }} />
       <AddButton icon={'forward'} onClick={() => { incrementPage(); refreshTweets(page + 1); }} disabled={tweets && tweets.length < 5}/>
