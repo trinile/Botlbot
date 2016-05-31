8c05f5e0f4647288432145c3b3731571c388a12c
Nathaniel Edwards <nedwards@gmail.com>
2016-05-31 14:27:11 -0700

    Create basic About component

---
+++ public/client/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Login from './Login';
+import Logo from './Logo';
+
+const About = () => (
+  <div>
+    <Logo />
+    <Login />
+    <p>botlbot lets you build your own twitterbots with a simple, easy-to-use visual editor.</p>
+    <p>botlbot was built by:</p>
+    <div>
+      <img src={require('../assets/blackgear.png')} />
+      <img src={require('../assets/blackgear.png')} />
+      <img src={require('../assets/blackgear.png')} />
+    <div>
+  </div>
+);
+
+export default About;
