6d4793181339190558946dd774b59ea2bee0f14d
Nathaniel Edwards <nedwards@gmail.com>
2016-06-01 18:15:38 -0700

    Complete about page styling and content

--- public/client/components/About.js
+++ public/client/components/About.js
@@ -60,22 +60,81 @@ class About extends Component {
           </div>
         </Element>
         <Element name="howItWorks" className={splashStyles.element}>
-          <p>botlbot lets you build your own twitterbots.</p>
+          <span>
+            <h2>How it works</h2>
+            <p>botlbot lets you create your own twitterbots, without having to write any code.</p>
+          </span>
+          <div className={splashStyles.sample}>
+            <img src={require('../assets/template.png')} />
+            <span className={splashStyles.sampletext}>
+              <p>Use the template builder to construct a frame for your tweets. </p>
+              <p>Pull in news articles, tweets from your network, emoji, or content from a variety of other sources.</p>
+            </span>
+          </div>
+          <div className={splashStyles.sample}>
+            <span className={splashStyles.sampletext}>
+              <p>We'll generate tweets for you as soon as you create a template, and we'll refresh your dashboard every half hour after that. </p>
+              <p>Edit your tweets, post them right away, or schedule them for later.</p>
+            </span>
+            <img src={require('../assets/tweet.png')} />
+          </div>
           <Link to="team" smooth={true} duration={500} >
             <RaisedButton label='the team'/>
           </Link>
+          <br/>
         </Element>
         <Element name="team" className={splashStyles.element}>
-          <p>botlbot was built by:</p>
-          <img src={require('../assets/littlegear.png')} className={styles.teamgear}/>
-          <img src={require('../assets/littlegear.png')} className={styles.teamgear}/>
-          <img src={require('../assets/littlegear.png')} className={styles.teamgear}/>
+          <br/>
+          <h2>botlbot was built by:</h2>
+          <div className={splashStyles.team}>
+            <span className={splashStyles.bio}>
+              <img src={require('../assets/littlegear.png')} className={styles.teamgear}/>
+              <img src={'https://avatars3.githubusercontent.com/u/5132757'} className={splashStyles.bioimage}/>
+              <p><span className={splashStyles.bioname}>Nathaniel Edwards</span> <br/> Software Engineer</p>
+              <span className={splashStyles.biobuttons}>
+                <a href="https://github.com/nthaniel/" >
+                  <img src={require('../assets/github.png')} />
+                </a>
+                <a href="https://www.linkedin.com/in/nthaniel" >
+                  <img src={require('../assets/linkedin.png')} />
+                </a>
+              </span>
+            </span>
+            <span className={splashStyles.bio}>
+              <img src={require('../assets/littlegear.png')} className={styles.teamgearreverse}/>
+              <img src={'https://avatars1.githubusercontent.com/u/13231400'} className={splashStyles.bioimage}/>
+              <p><span className={splashStyles.bioname}>Trini Le</span> <br/> Software Engineer</p>
+              <span className={splashStyles.biobuttons}>
+                <a href="https://github.com/trinile/">
+                  <img src={require('../assets/github.png')} />
+                </a>
+                <a href="https://www.linkedin.com/in/trinile">
+                  <img src={require('../assets/linkedin.png')} />
+                </a>
+              </span>
+            </span>
+            <span className={splashStyles.bio}>
+              <img src={require('../assets/littlegear.png')} className={styles.teamgear}/>
+              <img src={'https://avatars3.githubusercontent.com/u/15823817'} className={splashStyles.bioimage}/>
+              <p><span className={splashStyles.bioname}>Danny Tuñon</span> <br/> Software Engineer</p>
+              <span className={splashStyles.biobuttons}>
+                <a href="https://github.com/danieltunon">
+                  <img src={require('../assets/github.png')} />
+                </a>
+                <a href="https://www.linkedin.com/in/dannytunon">
+                  <img src={require('../assets/linkedin.png')} />
+                </a>
+              </span>
+            </span>
+          </div>
+          <br/>
           <div style={{display: 'flex'}}> 
-              <Login />
-              <Link to="intro" smooth={true} duration={500} >
-                <RaisedButton label='to top' style={{marginLeft: '1.5rem'}}/>
-              </Link>
-            </div>
+            <Login />
+            <Link to="intro" smooth={true} duration={500} >
+              <RaisedButton label='to top' style={{marginLeft: '1.5rem'}}/>
+            </Link>
+          </div>
+          <br/>
         </Element>
         <div>
         </div>
